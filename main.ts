a2cb178204239809f7890ff2cc402c7f1a4bb4fc
sidma64 <toprak.code@gmail.com>
2025-04-22 02:54:08 +0300

    Get a reply from Anthropic

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log(process.env.ANTHROPIC_API_KEY);
+import { ChatAnthropic } from "@langchain/anthropic";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
+const model = new ChatAnthropic({
+  model: "claude-3-7-sonnet-latest",
+  temperature: 0
+});
+
+const messages = [
+  new SystemMessage("Translate the following from English into Italian"),
+  new HumanMessage("hi!"),
+];
+
+(async () => console.log((await model.invoke(messages)).text))();
